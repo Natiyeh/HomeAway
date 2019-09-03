012110888cd8329ed86fc87e35769be1267e1aac
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-09-03 16:37:50 -0500

    CRUD homes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,10 +8,39 @@ class HomesController < ApplicationController
   end
 
   def new
-
+    @home = Home.new
   end
 
   def create
-    @home
+    @home = Home.new(home_params)
+    @home.save
+
+    redirect_to home_path(@home)
+  end
+
+  def edit
+    @home = Home.find(params[:id])
+  end
+
+  def update
+    @home = Home.find(params[:id])
+    @home.update(home_params)
+
+    redirect_to home_path(@home)
+  end
+
+  def destroy
+    @home = Home.find(params[:id])
+    @home.destroy
+
+    redirect_to homes_path
+  end
+
+  private
+
+  def home_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:home).permit(:name, :address)
   end
 end
