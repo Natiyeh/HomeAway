aed1369991a3ce8abd5889b799a051016ab2ec4e
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-10-27 18:37:22 -0500

    added shadows to profile page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-import { initAutocomplete } from '../plugins/init_autocomplete';
-import { initMapbox } from '../plugins/init_mapbox';
+// import { initAutocomplete } from '../plugins/init_autocomplete';
+// import { initMapbox } from '../plugins/init_mapbox';
 
-initAutocomplete();
-initMapbox();
+// initAutocomplete();
+// initMapbox();
+require('./maps')
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -24,7 +24,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { duration: 0, padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { duration: 5000, padding: 70, maxZoom: 15 });
 };
 
 const initMapbox = () => {
